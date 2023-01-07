982759391a39dc7b7cddf90975f848c8e0d26818
ugurgurel <ugurgurel@gmail.com>
2023-01-07 11:04:43 +0300

    details

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import MovieDetail from "../views/MovieDetail.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/movie/:id",
+    name: "Movie Detail",
+    component: MovieDetail,
   },
 ];
 
---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,60 @@
+Skip to content Search or jump to… Pull requests Issues Codespaces Marketplace
+Explore @ugurgurel TylerPottsDev / vue-movie-app Public Code Issues Pull
+requests Actions Projects Security Insights
+vue-movie-app/src/views/MovieDetail.vue @TylerPottsDev TylerPottsDev init Latest
+commit 3325ed7 on Jan 1, 2021 History 1 contributor 58 lines (50 sloc) 1.01 KB
+
+<template>
+  <div class="movie-detail">
+    <h2>{{ movie.Title }}</h2>
+    <p>{{ movie.Year }}</p>
+    <img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
+    <p>{{ movie.Plot }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import env from "@/env.js";
+export default {
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+    onBeforeMount(() => {
+      fetch(
+        `http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          movie.value = data;
+        });
+    });
+    return {
+      movie,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.movie-detail {
+  padding: 16px;
+  h2 {
+    color: #fff;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 16px;
+  }
+  p {
+    color: #fff;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+}
+</style>
